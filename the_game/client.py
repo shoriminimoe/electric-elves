4e7dd726c543b785f4f69f976ef74e36c1231b43
ChaseParate <97500441+ChaseParate@users.noreply.github.com>
2022-07-24 12:21:02 -0700

    Refactor drawing code

--- the_game/client.py
+++ the_game/client.py
@@ -11,11 +11,8 @@ def main() -> None:
 
     font = pygame.font.SysFont(None, 24)
 
-    prey_position = (300, 200)
-    prey_size = (10, 10)
-
-    hunter_position = (100, 500)
-    hunter_size = (40, 40)
+    prey = pygame.Rect(300, 200, 10, 10)
+    hunter = pygame.Rect(100, 500, 40, 40)
 
     messages = ["[SERVER] Test Message 1", "[SERVER] Test Message 2"]
     message_window = pygame.Rect(500, 0, 300, 600)
@@ -28,16 +25,16 @@ def main() -> None:
             if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
                 return
 
-        screen.fill((0, 0, 0))
+        screen.fill("black")
 
         # Draw Messages
         screen.fill((127, 127, 127), message_window)
-        screen.blit(font.render("Messages", True, (255, 255, 255)), (525, 25))
+        screen.blit(font.render("Messages", True, "white"), (525, 25))
         for i, message in enumerate(messages):
-            screen.blit(font.render(message, True, (200, 200, 200)), (525, 60 + i * 25))
+            screen.blit(font.render(message, True, "lightgray"), (525, 60 + i * 25))
 
-        pygame.draw.rect(screen, (0, 0, 255), (prey_position[0], prey_position[1], prey_size[0], prey_size[1]))
-        pygame.draw.rect(screen, (255, 0, 0), (hunter_position[0], hunter_position[1], hunter_size[0], hunter_size[1]))
+        pygame.draw.rect(screen, "red", prey)
+        pygame.draw.rect(screen, "blue", hunter)
         pygame.display.flip()
 
 
