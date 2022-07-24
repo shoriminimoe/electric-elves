29f4cec0f4356d223778cf0c8c19457e638c2e9d
ChaseParate <97500441+ChaseParate@users.noreply.github.com>
2022-07-24 12:17:36 -0700

    Draw messages to screen

--- the_game/client.py
+++ the_game/client.py
@@ -9,11 +9,16 @@ def main() -> None:
     screen_size = (800, 600)
     screen = pygame.display.set_mode(screen_size)
 
+    font = pygame.font.SysFont(None, 24)
+
     prey_position = (300, 200)
     prey_size = (10, 10)
 
-    hunter_position = (500, 500)
+    hunter_position = (100, 500)
     hunter_size = (40, 40)
+
+    messages = ["[SERVER] Test Message 1", "[SERVER] Test Message 2"]
+    message_window = pygame.Rect(500, 0, 300, 600)
 
     # TODO: Connect to the server!
 
@@ -24,6 +29,13 @@ def main() -> None:
                 return
 
         screen.fill((0, 0, 0))
+
+        # Draw Messages
+        screen.fill((127, 127, 127), message_window)
+        screen.blit(font.render("Messages", True, (255, 255, 255)), (525, 25))
+        for i, message in enumerate(messages):
+            screen.blit(font.render(message, True, (200, 200, 200)), (525, 60 + i * 25))
+
         pygame.draw.rect(screen, (0, 0, 255), (prey_position[0], prey_position[1], prey_size[0], prey_size[1]))
         pygame.draw.rect(screen, (255, 0, 0), (hunter_position[0], hunter_position[1], hunter_size[0], hunter_size[1]))
         pygame.display.flip()
