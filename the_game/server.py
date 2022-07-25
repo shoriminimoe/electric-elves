313053b8255e26204f108d7c8bda6512b7070d63
Sam Caldwell <shoriminimoe@pm.me>
2022-07-24 19:18:40 -0700

    Add message handling to server
    - Adds `MessageType` and `Message` classes for creating message objects.
    - Adds `process_message` function to define server-side logic for each
      message type

--- the_game/server.py
+++ the_game/server.py
@@ -1,6 +1,9 @@
 """Server module"""
 import asyncio
+import json
 import logging
+from collections import UserDict
+from enum import IntEnum, auto
 
 import websockets
 from websockets.exceptions import ConnectionClosedError
@@ -12,6 +15,89 @@ logging.basicConfig(
     datefmt="%Y-%m-%d %H:%M:%S",
 )
 LOG = logging.getLogger(__name__)
+
+
+class MessageType(IntEnum):
+    """MessageType enum
+
+    This enum represents all valid message types the server will process. To
+    add a new message type:
+
+        1. Add the name to this class definition
+        2. Add a case in the match statement of `process_message` to handle the
+           message type
+    """
+
+    MOVE = auto()
+
+
+class Message(UserDict):
+    """Message dictionary
+
+    Use the `serialize` method to prepare the message string for the websocket.
+    Use the `deserialize` classmethod to reconstruct a message object from the
+    websocket.
+    """
+
+    def __init__(self, type: MessageType | int, content: str):
+        if isinstance(type, int):
+            type = MessageType(type)
+
+        if type not in MessageType:
+            raise ValueError(f"{type} is not a valid MessageType")
+
+        LOG.debug(f"Message(type:{type}, content:{content}")
+        self._type_value = type.value
+        super().__init__(type=type, content=content)
+
+    def serialize(self):
+        """Return serialized string for message
+
+        Use this method to prepare the message to be sent throught the
+        websocket. For example::
+
+            >>> message = Message(type=MessageType.MOVE, content="up")
+            >>> socket.send(message.serialize())
+
+        """
+        return json.dumps(self.data)
+
+    @classmethod
+    def deserialize(cls, message: str):
+        """Construct a Message from a serialized Message
+
+        Use this method to construct a Message object from a serialized message
+        from the websocket. For example::
+
+            >>> message_str = await socket.recv()
+            >>> message = Message.deserialize(message_str))
+
+        This is the reverse operation of `serialize`. So this should always work::
+
+            >>> message = Message(type=MessageType.MOVE, content="up")
+            >>> assert message == Message.deserialize(message.serialize())
+        """
+        LOG.info(f"Creating new Message from message: {message}")
+        message_dict = json.loads(message)
+        try:
+            return cls(MessageType(message_dict["type"]), message_dict["content"])
+        except KeyError as exc:
+            raise ValueError(
+                f"message must include 'type' and 'content' keys: {message}"
+            ) from exc
+
+
+def process_message(message: Message) -> str:
+    """Process a client message
+
+    This is where the server-side business logic lives.
+    """
+    match message["type"]:
+        case MessageType.MOVE:
+            # TODO: something meaningful with the message content
+            return message["content"]
+        case _:
+            raise ValueError(f"invalid message type: {message['type']}")
 
 
 connected_clients: set[WebSocketServerProtocol] = set()
@@ -26,9 +112,14 @@ async def handler(websocket: WebSocketServerProtocol):
             LOG.info("message from %s: %s", websocket.id, message)
 
             # TODO handle game state updates
+            try:
+                result = process_message(Message.deserialize(message))
+            except ValueError as exc:
+                LOG.error(exc.args)
+                result = exc.args
 
             # send the message to all connected clients
-            websockets.broadcast(connected_clients, message)
+            websockets.broadcast(connected_clients, result)
 
     except ConnectionClosedError:
         # TODO handle the game state for disconnection errors
